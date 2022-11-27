9918897bb55cd9c01e628ff95778ec3da1c08b2e
Richa Kumari <86349597+richa2970@users.noreply.github.com>
2022-11-27 10:52:53 +0530

    Update index.php

--- onlinevotingsystem/index.php
+++ onlinevotingsystem/index.php
@@ -9,4 +9,4 @@ if (isset($_SESSION['SESS_NAME'])!="") {
 <p><center><legend><font color='#008000' size='18'>This system allows all registered users to vote for their favorite POLITICAL PARTY.</p>
 <p>In order to make a vote you have to register first and then login.</font></legend></center>
     <p>&nbsp;&nbsp;</p>
-<?php include "footer.php";?>
+<?php ?>
